1303254d7f4b7db44272ce821647e3620806083d
Oleg <schika145@gmail.com>
2023-10-18 10:13:58 +0500

    Работает коряво

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывод третьей цифры любого числа или сообщает что третьей цифры нет
+Console.WriteLine("Введите число");
+string aname = Console.ReadLine();
+int nam = Convert.ToInt32(aname);
+
+while (nam >= 100 &&  nam < 1000)
+{
+    nam = nam % 10;
+    Console.WriteLine(nam);
+    
+}
+ 
+    if (nam < 100) Console.WriteLine("Третьей цифры нет");
+    if (nam >= 1000 )
+    {
+        nam = nam / 10;
+    }
+   
